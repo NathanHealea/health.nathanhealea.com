6506143e1057310bd2884babd6b55447c59355da
Nathan Healea <nate.healea@gmail.com>
2023-08-03 08:11:44 -0700

    Initalize Strapi with Javascrtip; Remove Strapi Typescript

--- strapi/src/api/daylio-entry/controllers/daylio-entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * daylio-entry controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::daylio-entry.daylio-entry');
--- strapi/src/api/daylio-entry/routes/daylio-entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * daylio-entry router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::daylio-entry.daylio-entry');
